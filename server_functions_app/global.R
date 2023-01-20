53fff70c58240fc47996adc4118f733c34037786
samanthacsik <samanthacsik@gmail.com>
2023-01-19 22:42:55 -0800

    removed unused apps/updated names

--- server_functions_app/global.R
+++
@@ -1,6 +0,0 @@
-# SBC LTER Data: https://portal.edirepository.org/nis/mapbrowse?packageid=knb-lter-sbc.2002.30
-
-# LOAD LIBRARIES ----
-library(shiny)
-library(tidyverse)
-
--- test/global.R
+++
@@ -1,5 +0,0 @@
-# LOAD LIBRARIES ----
-library(shiny)
-library(shinydashboard)
-library(tidyverse)
-library(palmerpenguins)
